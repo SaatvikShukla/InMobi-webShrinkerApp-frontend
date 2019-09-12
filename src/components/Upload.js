4e79a13431412c3b5a49f8d24cd7803ad59c3f5c
Saatvik Shukla <me@saatvikshukla.com>
2019-09-12 16:04:59 +0530

    Setup inital routes and components

---
+++ src/components/Upload.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Upload extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Upload</h2>
+        </div>
+    );
+  }
+}
+
+export default Upload;
---
+++ src/components/View.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class View extends Component {
+  render() {
+    return (
+        <div>
+          <h2>View</h2>
+        </div>
+    );
+  }
+}
+
+export default View;
